c91630ea9e0d60a81ca4fbc93633beecbadd6315
liao <liaovq@gmail.com>
2023-07-07 14:05:45 +0800

    add error format

--- lib/main.dart
+++ lib/main.dart
@@ -116,6 +116,7 @@ class _MyHomePageState extends State<MyHomePage> {
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
             ),
+            Text('$_counter', style: Theme.of(context).textTheme.headlineMedium),
           ],
         ),
       ),
